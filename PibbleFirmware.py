137f791bf96acfeeb5dac5b3cbb5d01d447e2c05
Maxime <maxime.gras-chevalier@laposte.net>
2019-07-28 17:55:45 +0200

    Add base Flask api and PibbleDatabase class.

---
+++ PibbleFirmware.py
@@ -0,0 +1,64 @@
+from flask import Flask, jsonify
+from service import PibbleDatabase
+
+app = Flask(__name__)
+
+database = PibbleDatabase.PibbleDatabase()
+
+objs = [{
+    "OBJECT": "NGC 7831",
+    "OTHER": "IC 1530",
+    "TYPE": "GALXY",
+    "CON": "AND",
+    "RA": "00 07.3",
+    "DEC": "+32 37",
+    "MAG": "12,8",
+    "SUBR": "12,3",
+    "U2K": 89,
+    "TI": 4,
+    "SIZE_MAX": "1.5 m",
+    "SIZE_MIN": "0.3 m",
+    "PA": 38,
+    "CLASS": "Sb",
+    "NSTS": "",
+    "BRSTR": "",
+    "BCHM": "",
+    "NGC DESCR": "eF;vS;mE;vF*v nr",
+    "NOTES": ""
+  },
+  {
+    "OBJECT": "NGC    5",
+    "OTHER": "UGC    62",
+    "TYPE": "GALXY",
+    "CON": "AND",
+    "RA": "00 07.8",
+    "DEC": "+35 22",
+    "MAG": "13,3",
+    "SUBR": "13,2",
+    "U2K": 89,
+    "TI": 4,
+    "SIZE_MAX": "1.2 m",
+    "SIZE_MIN": "0.7 m",
+    "PA": 115,
+    "CLASS": "Elliptical",
+    "NSTS": "",
+    "BRSTR": "",
+    "BCHM": "",
+    "NGC DESCR": "vF;vS;N=*13;14",
+    "NOTES": "compact"
+  }]
+
+
+@app.route('/objects/<str:name>', methods=['GET'])
+def getObjectByName(name):
+    return jsonify(database.getObjectByName(name))
+
+@app.route('/command/track', methods=['GET'])
+def get_track():
+    print("track")
+    return jsonify(objs[0]["OTHER"])
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ service/PibbleDatabase.py
@@ -0,0 +1,59 @@
+import MySQLdb
+
+class PibbleDatabase:
+    def __init__(paramMysql=None):
+        if not paramMysql == None:
+            self.params = paramMysql
+        else:
+            self.params = = {
+                'host'   : 'localhost',
+                'user'   : 'root',
+                'passwd' : 'test',
+                'db'     : 'pibble_catalog'
+            }
+
+        try:
+            self.conn = MySQLdb.connect(**self.params)
+            self.cursor = conn.cursor()
+        except(Exception):
+            print("An error occured during connection to DB.")
+    
+    def getNames(self):
+        self.cursor.execute("SELECT * FROM objects")
+        names = self.cursor.fetchall()
+        return names
+
+    def getAllCollumns(self):
+        collNames = []
+        self.cursor.execute("SHOW COLUMNS FROM objects")
+        coll = self.cursor.fetchall()
+        for collumn in coll:
+            collNames.append(collumn[0])
+        return collNames
+
+    def getObjectByName(self, name):
+        self.cursor.execute("SELECT * FROM objects WHERE NAME = {}".format(name))
+        row = self.cursor.fetchall()
+        return row
+
+"""paramMysql = {
+    'host'   : 'localhost',
+    'user'   : 'root',
+    'passwd' : 'test',
+    'db'     : 'pibble_catalog'
+}
+
+sql = "SELECT * FROM objects"
+sql2 = "SHOW TABLES"
+sql3 = "SHOW COLUMNS FROM objects"
+
+conn = MySQLdb.connect(**paramMysql)
+
+cur = conn.cursor()
+cur.execute(sql3)
+
+rows = cur.fetchall()
+print(rows)
+
+conn.close()
+"""
